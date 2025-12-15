f6f6ed3fad9b1401117b7392b730c053af5bdafb
abnermc <ab.medina.011@gmail.com>
2025-12-15 13:20:07 -0500

    Se dio funcionamiento al fomrulario de búsqueda y filtro de tecnología

--- react-vite-jscamp/src/App.jsx
+++ react-vite-jscamp/src/App.jsx
@@ -13,10 +13,27 @@ import './App.css'
 const RESULTS_PER_PAGE = 5
 
 function App() {
+  const [filters, setFilters] = useState({
+    technology: '',
+    location: '',
+    experienceLevel: ''
+  })
+  const [textFilter, setTextFilter] = useState('')  
   const [currentPage, setCurrentPage] = useState(1)
-  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE)
 
-  const pagedResults = jobsData.slice(
+  const jobsFilteredByFilters = jobsData.filter(job =>{
+    return (
+      (filters.technology === '' || job.data.technology === filters.technology.toLowerCase()) 
+    )
+  })
+
+  const jobsWithTextFilter = textFilter === '' ? jobsFilteredByFilters : jobsFilteredByFilters.filter(job =>{
+    return job.titulo.toLowerCase().includes(textFilter.toLowerCase())
+  })
+
+  const totalPages = Math.ceil(jobsWithTextFilter.length / RESULTS_PER_PAGE)
+
+  const pagedResults = jobsWithTextFilter.slice(
     (currentPage - 1) * RESULTS_PER_PAGE,
     currentPage * RESULTS_PER_PAGE
   )
@@ -26,11 +43,19 @@ function App() {
     setCurrentPage(page)
   }
 
+  const handleSearch = (filters) =>{
+    setFilters(filters)
+    setCurrentPage(1)
+  }  
+  const handleTextFilter = (newTextoToFilter)=>{
+    setTextFilter(newTextoToFilter)
+    setCurrentPage(1)
+  }
   return (  
     <>
       <Header/>
       <main>
-        <SearchFormSection/>
+        <SearchFormSection onSearch={handleSearch} onTextFilter={handleTextFilter}/>
         <section>
           <JobsListings jobs={pagedResults}/>
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePagesChange} />
