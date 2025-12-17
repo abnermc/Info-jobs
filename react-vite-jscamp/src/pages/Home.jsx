94690e1573c0c434c14b73aef6a266d860471e1d
abnermc <ab.medina.011@gmail.com>
2025-12-17 16:58:52 -0500

    se dió funcionalidad al botón del buscador

--- react-vite-jscamp/src/pages/Home.jsx
+++ react-vite-jscamp/src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import { useRouter } from "../hooks/useRouter"
 export function HomePage(){
+  const {navigateTo} = useRouter()  
+  const handleSearch = (event) =>{
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const searchTerm = formData.get('search')
+
+    const url = searchTerm
+      ? `/search?text=${encodeURIComponent(searchTerm)}`
+      : '/search'
+    
+    navigateTo(url)
+  }
     return(
         <main>
             <section>
@@ -8,10 +21,10 @@ export function HomePage(){
             
               <p>Únete a la comunidad más grande de desarrolladores y encuentra tu próxima oportunidad.</p>
             
-              <form role="search">
+              <form role="search" name="search-form" onSubmit={(handleSearch)}>
                 <div>
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"
+                    stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
                     class="icon icon-tabler icons-tabler-outline icon-tabler-search">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
