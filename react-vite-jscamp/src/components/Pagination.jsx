0ff6b093310f53fee638a251fadf4ec9c288c962
abnermc <ab.medina.011@gmail.com>
2025-12-29 18:28:26 -0500

    Fix a small typo

--- react-vite-jscamp/src/components/Pagination.jsx
+++ react-vite-jscamp/src/components/Pagination.jsx
@@ -35,27 +35,40 @@ export function Pagination( { currentPage = 1, totalPages = 10, onPageChange } )
 
     return(
         <nav className={styles.pagination}>
-            <a href={buildPageUrl(currentPage-1)} style={stylePrevButton} onClick={handlePrevClick}><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+            <a 
+                href={buildPageUrl(currentPage-1)} 
+                style={stylePrevButton} 
+                onClick={handlePrevClick}>
+                    
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
                 strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M15 6l-6 6l6 6" />
-            </svg></a>
+                </svg>
+            </a>
             {pages.map(page => (
                 <a  
                     key={page}
-                    href={buildPageUrl(currentPage-1)}
+                    data-page={page}
+                    href={buildPageUrl(page)}
                     className={currentPage === page ? styles.isActive : ''}
                     onClick={()=> handleChangePage(event, page)}
                 >
                 {page}
                 </a>
             ))}
-            <a href={buildPageUrl(currentPage+1)} style={styleNextButton} onClick={handleNextClick}><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
+            <a 
+                href={buildPageUrl(currentPage+1)} 
+                style={styleNextButton} 
+                onClick={handleNextClick}>
+
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
                 strokeLinecap="round" strokeLinejoin="round"
                 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M9 6l6 6l-6 6" />
-            </svg></a>
+                </svg>
+            </a>
         </nav>
     )
 }
