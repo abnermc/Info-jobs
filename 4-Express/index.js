e21d4c89b5662260550a60bc22e6fa5196166426
abmed <ab.medina.011@gmail.com>
2026-01-19 12:19:38 -0500

    Implement code changes to enhance functionality and improve performance

--- 4-Express/index.js
+++ 4-Express/index.js
@@ -1,23 +1,71 @@
 import express from 'express'
+import jobs from './jobs.json' with { type: 'json' }
+import { DEFAULTS } from './config.js'
 
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? DEFAULTS.PORT
 const app = express()
 
-app.use((request, response, next)=>{
+app.use((req, res, next)=>{
     const timeString = new Date().toLocaleDateString()
-    console.log(`[${timeString}] ${request.method} ${request.url}`)
+    console.log(`[${timeString}] ${req.method} ${req.url}`)
     next()
 })
-app.get('/',(request, response)=>{
-    response.send('Hello World')
+
+app.get('/',(req, res)=>{
+    res.send('Hello World')
 })
-app.get('/health',(request,response)=>{
-    return response.json({
+
+app.get('/health',(req, res)=>{
+    return res.json({
         status: 'ok',
         uptime: process.uptime()
     })
 })
 
+app.get('/jobs',(req, res)=>{
+    const {text, title, level, limit = DEFAULTS.LIMIT_PAGINATION, technology, offset = DEFAULTS.LIMIT_OFFSET} = req.query
+
+    let filteredJobs = jobs
+
+    if(text){
+        const searchTerm = text.toLowerCase()
+        filteredJobs = filteredJobs.filter(job=>
+            job.titulo.toLowerCase().includes(searchTerm) || job.descripcion.toLowerCase().includes(searchTerm)
+        )
+    }
+
+    const limitNumber = Number(limit)
+    const offsetNumber = Number(offset)
+
+    const paginatedJobs = filteredJobs.slice(offsetNumber, offsetNumber+limitNumber)
+
+    return res.json(paginatedJobs)
+})
+
+app.get('/jobs/:id', (req, res)=>{
+    const { id } = req.params
+
+    const job = jobs.find()
+    return res.json({
+        job: {id, title: `Job with id: ${id}`}
+    })
+})
+
+app.put('/jobs:id', (req,res)=>{
+
+})
+
+app.patch('/jobs:id', (req,res)=>{
+
+})
+
+app.post('/jobs:id', (req,res)=>{
+
+})
+
+app.delete('/jobs:id', (req, reqs)=>{
+
+})
 app.listen(PORT, () =>{
-    console.log(`Servidos levantado en http://localhost:${PORT}`)
+    console.log(`Servidor levantado en http://localhost:${PORT}`)
 })
