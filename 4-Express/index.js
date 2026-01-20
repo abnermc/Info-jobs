3e586dd68fdf8bfdde94527738b8ad5511c40a5a
abmed <ab.medina.011@gmail.com>
2026-01-19 21:44:19 -0500

    feat: add CORS support and update job fetching logic

--- 4-Express/index.js
+++ 4-Express/index.js
@@ -1,9 +1,26 @@
 import express from 'express'
 import jobs from './jobs.json' with { type: 'json' }
 import { DEFAULTS } from './config.js'
+import cors from 'cors'
 
 const PORT = process.env.PORT ?? DEFAULTS.PORT
 const app = express()
+
+const ACCEPTED_ORIGINS = [
+    'http://localhost:1234',
+    'http://localhost:5173'
+]
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true)
+            if(ACCEPTED_ORIGINS.includes(origin)) return callback(null, true)
+            return callback(new Error('Origin not allowed '))
+        }
+    })
+)
+app.use(express.json())
 
 app.use((req, res, next)=>{
     const timeString = new Date().toLocaleDateString()
@@ -39,16 +56,17 @@ app.get('/jobs',(req, res)=>{
 
     const paginatedJobs = filteredJobs.slice(offsetNumber, offsetNumber+limitNumber)
 
-    return res.json(paginatedJobs)
+    return res.json({ data: paginatedJobs, total: filteredJobs.length, limit: limitNumber, offset: offsetNumber })
 })
 
 app.get('/jobs/:id', (req, res)=>{
     const { id } = req.params
 
-    const job = jobs.find()
-    return res.json({
-        job: {id, title: `Job with id: ${id}`}
-    })
+    const job = jobs.find(job => job.id === id)
+
+    if(!job) return res.status(404).json({ error: 'Job not found'})
+    
+    return res.json(job)
 })
 
 app.put('/jobs:id', (req,res)=>{
@@ -59,8 +77,20 @@ app.patch('/jobs:id', (req,res)=>{
 
 })
 
-app.post('/jobs:id', (req,res)=>{
+app.post('/jobs', (req,res)=>{
+    const { titulo, empresa, ubicacion, data} = req.body
 
+    const newJob ={
+        id: crypto.randomUUID(),
+        titulo,
+        empresa,
+        ubicacion,
+        data
+    }
+
+    jobs.push(newJob) //Proximamente en una base de datos
+
+    return res.status(201).json(newJob)
 })
 
 app.delete('/jobs:id', (req, reqs)=>{
