e2b251bdc1959f7d0738c75a46c07ef5e2d71d2e
abmed <ab.medina.011@gmail.com>
2026-01-27 11:33:10 -0500

    i don't really know

--- 4-Express/app.js
+++ 4-Express/app.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 app.use('/jobs', jobsRouter)
 
-if(process.env.NODE_ENV !== 'production'){
+if(process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test'){
     app.listen(PORT, () =>{
     console.log(`Servidor levantado en http://localhost:${PORT}`)
 })
