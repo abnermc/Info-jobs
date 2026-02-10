b332fcbd962eaa5774317dd99e43ebdd6e4addf7
abmed <ab.medina.011@gmail.com>
2026-02-09 21:43:15 -0500

    Install Zod for validation

--- 4-Express/routes/jobs.js
+++ 4-Express/routes/jobs.js
@@ -1,11 +1,20 @@
 import { Router } from "express"
 import { JobController } from "../controllers/jobs.js"
+import { validateJob, validatePartialJob } from "../schemas/jobs.js"
 
 export const jobsRouter = Router()
 
+function validateCreate(req, res, next){
+    const result = validateJob(req.body)
+    if(result.success){
+        req.body = result.data
+    }
+    return res.status(400).json({error: 'Invalid request', details: result.error.errors})
+}
+
 jobsRouter.get('/', JobController.getAll)
 jobsRouter.get('/:id', JobController.getId)
-jobsRouter.post('/', JobController.create)
+jobsRouter.post('/', validateCreate, JobController.create)
 jobsRouter.put('/:id', JobController.update)
 jobsRouter.patch('/:id', JobController.partialUpdate)
 jobsRouter.delete('/:id', JobController.delete)
